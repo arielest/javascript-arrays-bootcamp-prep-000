2bfd864c0d12aab94bb779e3c0e95aa571e7249a
cool-library-2371 <est.ariel@gmail.com>
2020-03-28 18:29:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"] ;
 
-function = addElementToBeginningOfArray
+function addElementToBeginningOfArray(chocolateBars, candyString){
+  return [candyString, ...chocolateBars];
+}
+ 
+function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
+  chocolateBars.unshift(candyString);
+  return chocolateBars;
+}
+ 
